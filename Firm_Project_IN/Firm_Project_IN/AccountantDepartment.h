7bbd1ff1d91d7ad17c2dd5f0024508bf7d91f0e6
Ivailo <paradoxa624@gmail.com>
2020-08-14 11:16:12 +0300

    Added departments

---
+++ Firm_Project_IN/Firm_Project_IN/AccountantDepartment.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Department.h"
+class AccountantDepartment : public Department
+{
+
+};
+
---
+++ Firm_Project_IN/Firm_Project_IN/Department.h
@@ -0,0 +1,8 @@
+#pragma once
+class Department
+{
+public:
+	virtual void giveTask() = 0;
+	virtual void finishTask() = 0;
+};
+
