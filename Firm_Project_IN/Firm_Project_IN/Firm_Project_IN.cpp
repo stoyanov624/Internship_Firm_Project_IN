75acb05eaa121b3df87e4b9fa2ee9b67870e420b
Nakahotty <naskuuu@abv.bg>
2020-08-14 11:25:48 +0300

    Changes in main.cpp

--- Firm_Project_IN/Firm_Project_IN/Firm_Project_IN.cpp
+++ Firm_Project_IN/Firm_Project_IN/Firm_Project_IN.cpp
@@ -1,8 +1,19 @@
-
-
 #include <iostream>
+#include "Accountable.h"
+#include "Seller.h"
+#include "Accountant.h"
+#include "Programer.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	std::vector<std::string> languages = { "C++", "JavaScript", "C#" };
+	Employee* nasko = new Programmer("NASKO", languages, 16);
+	Employee* mladen = new Seller("MLADEN");
+	Employee* traicho = new Accountant("TRAICHO");
+
+	std::vector<Employee*> employees;
+	employees.push_back(nasko);
+	employees.push_back(mladen);
+	employees.push_back(traicho);
+
+	Accountable::getInstance(employees)->report();
 }
